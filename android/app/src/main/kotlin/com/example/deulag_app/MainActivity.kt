4827573e44a47e0cc0e284f12602ccb37fcadc62
Eros Borba <borba.eros@gmail.com>
2020-11-09 20:28:59 -0300

    add initial files

---
+++ android/app/src/main/kotlin/com/example/deulag_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deulag_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'SF Pro Display'),
+      title: 'Buy Tickets',
+      home: HomePage(),
+    );
+  }
+}
